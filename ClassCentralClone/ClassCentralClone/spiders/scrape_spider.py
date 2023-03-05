35ac1f6bb35b0b7a55e29750917e7eb911148a86
Ilerioluwa Albert Akinola <ilerioluwaakinola@gmail.com>
2023-03-05 13:10:21 +0200

    Initial Commit

---
+++ ClassCentralClone/ClassCentralClone/spiders/scrape_spider.py
@@ -0,0 +1,49 @@
+import scrapy
+
+from scrapy.linkextractors import LinkExtractor
+from scrapy.http import Request
+from pathlib import Path
+from ..items import ClasscentralcloneItem
+
+
+class WebpageSpider(scrapy.Spider):
+    name = "webpage"
+    allowed_domains = ["www.classcentral.com"]
+
+    start_urls = [
+        'https://www.classcentral.com/'
+    ]
+
+    def parse(self, response):
+        # Link Extractor
+        page_urls = LinkExtractor(allow_domains=(r"universities/",
+                                                 r"university/",
+                                                 r"providers/",
+                                                 r"provider/",
+                                                 r"institutions/",
+                                                 r"institution/",
+                                                 r"report/",
+                                                 r"subjects/",
+                                                 r"subject/",
+                                                 r"collection/",
+                                                 r"rankings/",
+                                                 r"help/",),
+                                  tags=("a", "img", "buttons")).extract_links(response)
+        for page in page_urls:
+            yield Request(page.url, callback=self.parse_item)
+
+    def parse_item(self, response):
+
+        # Saving the pages
+        page = response.url.split("/")[-2]
+        filename = f'{page}.html'
+        Path(filename).write_bytes(response.body)
+
+        # Saving the images
+        img_url = response.css("img").xpath("@src").get()
+        svg_url = response.css("src").xpath("@src").get()
+        item = ClasscentralcloneItem()
+        item["image_urls"] = [img_url]
+        item["image_urls"] = [svg_url]
+        yield item
+
---
+++ ClassCentralClone/ClassCentralClone/spiders/webpage_scrape.py
@@ -0,0 +1,47 @@
+import scrapy
+
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+from pathlib import Path
+from ..items import ClasscentralcloneItem
+
+
+class WebpageScrapeSpider(CrawlSpider):
+    name = "webpage_scrape"
+    allowed_domains = ["www.classcentral.com"]
+    start_urls = ["http://www.classcentral.com/"]
+
+    rules = (
+        Rule(LinkExtractor(allow_domains=(r"universities/",
+                                          r"university/",
+                                          r"providers/",
+                                          r"provider/",
+                                          r"institutions/",
+                                          r"institution/",
+                                          r"report/",
+                                          r"subjects/",
+                                          r"subject/",
+                                          r"collection/",
+                                          r"rankings/",
+                                          r"help/",
+                                          )
+                           ), callback="parse_item", follow=True),
+    )
+
+    def parse_item(self, response):
+        page = response.url.split("/")[-2]
+        filename = f'{page}.html'
+        Path(filename).write_bytes(response.body)
+
+        # Saving the images
+        img_url = response.css("img").xpath("@src").get()
+        svg_url = response.css("src").xpath("@src").get()
+        item = ClasscentralcloneItem()
+        item["image_urls"] = [img_url]
+        item["image_urls"] = [svg_url]
+
+        # item = {}
+        # item["domain_id"] = response.xpath('//input[@id="sid"]/@value').get()
+        # item["name"] = response.xpath('//div[@id="name"]').get()
+        # item["description"] = response.xpath('//div[@id="description"]').get()
+        return item
